bd166dbcab7ab8af99aba419a2f9724f011ee2f5
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-01-16 17:59:41 +0200

    1.6: add App, Reviews, Review components

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Reviews from '../reviews';
+// import {
+//   normalizedManagers,
+//   normalizedReviews,
+//   normalizedUsers,
+// } from '../../fixtures';
+
+const App = () => (
+  <div>
+    <Reviews />
+  </div>
+);
+
+export default App;
---
+++ src/components/reviews/review/review.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Review = () => <h2>Review</h2>;
+
+export default Review;
