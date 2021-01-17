6511546c544d3780d8dc9356c4891100d1516c2b
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-01-17 13:29:54 +0200

    1.11: add events for submitting new review

--- src/components/reviews/review-form/review-form.js
+++ src/components/reviews/review-form/review-form.js
@@ -6,9 +6,24 @@ const ReviewForm = () => {
   return (
     <div className={styles.reviewFormContainer}>
       <form className={styles.reviewForm}>
-        <textarea className={styles.message} />
+        <textarea
+          className={styles.message}
+          onKeyDown={(ev) => {
+            if (ev.key === 'Enter' && ev.ctrlKey) {
+              console.log('Ctrl + Enter pressed');
+            }
+          }}
+        />
         <div>
-          <button className={styles.submitButton}>Написать консультанту</button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              console.log('Submit button clicked!');
+            }}
+            className={styles.submitButton}
+          >
+            Написать консультанту
+          </button>
         </div>
       </form>
     </div>
